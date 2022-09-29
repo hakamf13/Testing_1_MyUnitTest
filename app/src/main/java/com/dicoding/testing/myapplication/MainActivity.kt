391b078b18f8840dc9a072221295d4fd15448713
Muhammad Hakam Fardana <mhakamf13@gmail.com>
2022-09-29 07:58:22 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/testing/myapplication/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.dicoding.testing.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import com.dicoding.testing.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val activityMainBinding get() = _activityMainBinding!!
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        mainViewModel = MainViewModel(CuboidModel())
+
+        activityMainBinding.btnSave.setOnClickListener(this)
+        activityMainBinding.btnCalculateSurfaceArea.setOnClickListener(this)
+        activityMainBinding.btnCalculateVolume.setOnClickListener(this)
+        activityMainBinding.btnCalculateCircumference.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val length = activityMainBinding.edtLength.text.toString().trim()
+        val width = activityMainBinding.edtWidth.text.toString().trim()
+        val height = activityMainBinding.edtHeight.text.toString().trim()
+
+        when {
+            TextUtils.isEmpty(length) -> {
+                activityMainBinding.edtLength.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(width) -> {
+                activityMainBinding.edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(height) -> {
+                activityMainBinding.edtHeight.error = "Field ini tidak boleh kosong"
+            }
+
+            else -> {
+                val valueLength = length.toDouble()
+                val valueWidth = width.toDouble()
+                val valueHeight = height.toDouble()
+
+                when (v.id) {
+                    R.id.btn_save -> {
+                        mainViewModel.save(valueLength, valueWidth, valueHeight)
+                        visible()
+                    }
+                    R.id.btn_calculate_circumference -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getCircumference().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_surface_area -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getSurfaceArea().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_volume -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getVolume().toString()
+                        gone()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
+    }
+
+    private fun visible() {
+        activityMainBinding.btnCalculateVolume.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateCircumference.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.VISIBLE
+        activityMainBinding.btnSave.visibility = View.GONE
+    }
+
+    private fun gone() {
+        activityMainBinding.btnCalculateVolume.visibility = View.GONE
+        activityMainBinding.btnCalculateCircumference.visibility = View.GONE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.GONE
+        activityMainBinding.btnSave.visibility = View.VISIBLE
+    }
+}
